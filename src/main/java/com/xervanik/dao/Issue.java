667a04f9756427d70d8a5f2b9cff31cecdb0be94
John Pettit <john.pettit@tridonic.com>
2018-10-16 08:16:23 -0700

    Basic fields added to POJOs

--- src/main/java/com/xervanik/dao/Issue.java
+++ src/main/java/com/xervanik/dao/Issue.java
@@ -1,7 +1,11 @@
 package com.xervanik.dao;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import java.util.Date;
+
 /**
  * <h1>Issue</h1>
  * Issue class for the things to be tracked
@@ -13,12 +17,56 @@ import javax.persistence.OneToOne;
 @Entity
 public class Issue extends Base {
 
+    @Id
+    @GeneratedValue
+    private Long id;
     private String title;
-
+    private String description;
+    private Date createDate;
     @OneToOne
     private User ownerId;
 
     public Issue() {
         super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public User getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(User ownerId) {
+        this.ownerId = ownerId;
     }
 }
--- src/main/java/com/xervanik/dao/User.java
+++ src/main/java/com/xervanik/dao/User.java
@@ -1,6 +1,8 @@
 package com.xervanik.dao;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 /**
  * <h1>User</h1>
@@ -14,6 +16,9 @@ import javax.persistence.Entity;
 @Entity
 public class User extends Base{
 
+    @Id
+    @GeneratedValue
+    private Long id;
     private String firstName;
     private String lastName;
     private String email;
@@ -23,5 +28,43 @@ public class User extends Base{
 
     public User() {
         super();
+    }
+
+    public User(String fName, String lName, String em) {
+        this.firstName = fName;
+        this.lastName = lName;
+        this.email = em;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
