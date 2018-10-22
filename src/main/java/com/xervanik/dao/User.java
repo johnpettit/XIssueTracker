40d560cdb5dde10097e99f83e109642ba181a5cb
John Pettit <john.pettit@cox.net>
2018-10-22 05:46:44 -0700

    Adding sec user

--- src/main/java/com/xervanik/dao/User.java
+++ src/main/java/com/xervanik/dao/User.java
@@ -24,13 +24,10 @@ public class User extends Base{
     private String lastName;
     @Column(nullable = false, unique = true)
     private String email;
-
-
-
     private String password;
 
 /*    @ManyToMany
-    private List<Skill> skills;*/
+    private List<Issue> issues;*/
 
     public User() {
         super();
